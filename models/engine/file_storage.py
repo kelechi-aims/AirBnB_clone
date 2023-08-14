d2dc6abceb2e2b7a01307a543e1ef9596eea0fae
anoti <anotibills@gmail.com>
2023-08-14 01:55:51 +0100

    File Storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,45 +1,26 @@
 #!/usr/bin/python3
-'''
-Module for the class FileSorage
-'''
+"""Object converter Module."""
 import json
 from os.path import exists
 
 
 class FileStorage:
-    '''
-    This class handles the serializes instances and deserializes
-    of instances to/from a JSON file.
-
-    Attributes:
-        __file_path (str): Path to the JSON file
-        __objects (dict): Dictionary to store objects b class name and id
-
-    '''
+    """Class that handles object conversion"""
 
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        '''
-        Returns the dictionary of objects.
-        '''
+        """Returns the dictionary of objects."""
         return FileStorage.__objects
 
     def new(self, obj):
-        '''
-        Adds a new object to the dictionary.
-
-        Args:
-            obj (BaseModel): The object to be added.
-        '''
+        """Object format"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
-        '''
-        Serialises and saves the objects to the JSON file.
-        '''
+        """Serialises and saves the objects to the JSON file."""
         serialized = {}
         for k, v in FileStorage.__objects.items():
             serialized[k] = v.to_dict()
@@ -47,9 +28,7 @@ class FileStorage:
             json.dump(serialized, file)
 
     def reload(self):
-        '''
-        Deserializes and loads the objects from the JSON file.
-        '''
+        """Deserializes and loads the objects from the JSON file."""
         from models.base_model import BaseModel
         from models.user import User
         from models.state import State
